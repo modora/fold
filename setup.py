2bd47726b1765d9924343df977d68fca72fc2311
Stanley Wu <stanleywu@scrappylabs.dev>
2021-12-22 11:15:49 -0800

    added non-core packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="fold",
     version="21.12.0",
-    packages=["fold", "fold.core"],
+    packages=["fold", "fold.core", "fold.config", "fold.output"],
     install_requires=[
         "loguru",
         "pyfunctional",
