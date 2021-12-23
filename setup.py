da3318710fc2c1732fd186bfb7c78e5231c825d7
Stanley Wu <stanleywu@scrappylabs.dev>
2021-12-23 05:24:32 +0000

    add python version requirement

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="fold",
     version="21.12.0",
+    python_requires=">=3.10",
     packages=["fold", "fold.core", "fold.config", "fold.output", "fold.utils"],
     install_requires=[
         "loguru",
