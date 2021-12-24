51622fbdd9215400de3223d8c70d22ea609cfd18
Stanley Wu <stanleywu@scrappylabs.dev>
2021-12-24 06:39:38 +0000

    add cache to plugin manager

--- fold/core/plugin.py
+++ fold/core/plugin.py
@@ -1,13 +1,9 @@
-from typing import Optional as _Optional, List as _List
+from typing import Optional as _Optional, List as _List, Dict as _Dict
 import importlib
-from fold.utils import loadObjectDynamically as _loadObjectDynamically
+import fold.utils as _utils
 
 
 class Plugin:
-    pass
-
-
-class PluginNotFoundError(AttributeError):
     pass
 
 
@@ -19,44 +15,66 @@ class PluginManager:
             obj (Plugin): The plugin class to manage
         """
         self._plugin = obj
+        self._cache: _Dict[str, Plugin] = {}
 
-    def load(self, name: str, package: _Optional[str] = None) -> Plugin:
+    @property
+    def cache(self) -> Plugin:
+        return self._cache
+
+    @cache.setter
+    def cache(self, value):
+        # use Plugin.NAME attribute if exists, otherwise, default to the class name
+        name = getattr(value, "NAME", value.__name__)
+        self._cache[name] = value
+
+    def load(
+        self, name: str, package: _Optional[str] = None, cache: _Optional[bool] = True
+    ) -> Plugin:
         """Dynamically load a module and import a plugin
 
         Args:
             name (str): Path to object in <module>.<object> notation
             package (str, optional): Required only if the module name is relative. Defaults to none.
+            cache (bool, optional): Use cache result
 
         Returns:
             Plugin: Plugin object loaded
 
         Raises:
-            PluginNotFoundError: The plugin does not exist in the module
             TypeError: Object loaded is not a subclass of the plugin
             ImportError: Failed to load the module
         """
+
+        if cache and name in self._cache:
+            return self._cache[name]
         try:
-            obj = _loadObjectDynamically(name, package)
+            obj = _utils.loadObjectDynamically(name, package)
         except AttributeError as e:
-            raise PluginNotFoundError("Plugin does not exist") from e
+            raise ImportError("Plugin does not exist") from e
 
         if not isinstance(obj, self._plugin):
             raise TypeError("Object is not a plugin")
 
-    def discover(self, module: str) -> _List[Plugin]:
+        self._cache = obj
+
+        return obj
+
+    def discover(self, module: str, cache: _Optional[bool] = True) -> _List[Plugin]:
         """Dynamically load a module and return a list of all plugin objects found
 
         Args:
             module (str): Name of the module to load
+            cache (bool, optional): Use cache result
 
         Returns:
             List[Plugin]: List of plugins discovered in the module
         """
 
         m = importlib.import_module(module)
-        # Return a list of non-private, plugin objects
-        return [
-            obj
-            for obj in dir(m)
-            if not obj.__name__.startswith("_") and issubclass(obj, self._plugin)
-        ]
+        # Check non-private objects whether they are Plugin objects
+        plugins: _List[Plugin] = []
+        for obj in [getattr(m, name) for name in dir(m) if not name.startswith("_")]:
+            if issubclass(obj, self._plugin):
+                self._cache = obj
+                plugins.append(obj)
+        return plugins
