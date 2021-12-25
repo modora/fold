acf22d45431a97edd50a46f1f8c762f194c2e49b
Stanley Wu <stanleywu@scrappylabs.dev>
2021-12-25 05:36:45 +0000

    add stdout output plugin

--- fold/output/stdout.py
+++ fold/output/stdout.py
@@ -1,12 +1,18 @@
-from typing import Any
+from typing import Any, Optional
 import sys
 
-from .common import OutputPlugin
+from .common import OutputPlugin, Content
 
 
 class StdoutOutputPlugin(OutputPlugin):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    NAME = "stdout"
+
+    def __init__(self, config: Optional[Content]) -> None:
+        super().__init__(config)
+
+    @classmethod
+    def parseConfig(cls, config: Optional[Content]) -> Optional[Content]:
+        return super().parseConfig(config)
 
     def write(self, data: Any):
         sys.stdout.write(str(data) + "\n")
